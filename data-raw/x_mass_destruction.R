e351cd2f14d671ce8144a6876e8f0a78e3004c50
Harriet Goers <h.goers@hotmail.com>
2024-04-03 11:59:56 -0400

    Add Fuhrmann 2008

---
+++ data-raw/x_mass_destruction.R
@@ -0,0 +1,22 @@
+x_mass_destruction_raw <- rio::import("https://files.prio.org/Journals/JPR/2008/45/5/Replication%20Fuhrmann.zip",
+                                      which = 1)
+
+x_mass_destruction <- x_mass_destruction_raw |>
+  dplyr::select(country,
+                ccode,
+                x_dual_use = expdu,
+                x_dual_use_licence = dulicense,
+                log_gdp = log_gdp2,
+                log_population = log_pop,
+                log_distance = log_dist,
+                log_x_all = logexpall,
+                defence_ally = ally_def,
+                democracy = dem7,
+                conflict = armforce,
+                pol_dissimilarity = swt_low10pct,
+                wmd_acquisition = wmd_acq,
+                wmd_pursuit)
+
+x_mass_destruction
+
+usethis::use_data(x_mass_destruction, overwrite = TRUE)
