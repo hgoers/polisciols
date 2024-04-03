15fa3d3e27a30512305c9abf39e893f2639d319b
Harriet Goers <h.goers@hotmail.com>
2024-04-03 17:36:34 -0400

    Make the trade value in terms of billions

--- data-raw/x_mass_destruction.R
+++ data-raw/x_mass_destruction.R
@@ -2,20 +2,22 @@ x_mass_destruction_raw <- rio::import("https://files.prio.org/Journals/JPR/2008/
                                       which = 1)
 
 x_mass_destruction <- x_mass_destruction_raw |>
-  dplyr::select(country,
-                ccode,
-                x_dual_use = expdu,
-                x_dual_use_licence = dulicense,
-                log_gdp = log_gdp2,
-                log_population = log_pop,
-                log_distance = log_dist,
-                log_x_all = logexpall,
-                defence_ally = ally_def,
-                democracy = dem7,
-                conflict = armforce,
-                pol_dissimilarity = swt_low10pct,
-                wmd_acquisition = wmd_acq,
-                wmd_pursuit)
+  dplyr::transmute(
+    country,
+    ccode,
+    x_dual_use = expdu / 1e9,
+    x_dual_use_licence = dulicense,
+    log_gdp = log_gdp2,
+    log_population = log_pop,
+    log_distance = log_dist,
+    log_x_all = logexpall,
+    defence_ally = ally_def,
+    democracy = dem7,
+    conflict = armforce,
+    pol_dissimilarity = swt_low10pct,
+    wmd_acquisition = wmd_acq,
+    wmd_pursuit
+  )
 
 x_mass_destruction
 
