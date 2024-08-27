fe3b81728ed32967145ba6bd2a061d40a06c299b
Harriet Goers <h.goers@hotmail.com>
2024-08-27 17:39:06 -0400

    Add NES data for week 1

---
+++ R/nes.R
@@ -0,0 +1,17 @@
+#' American National Election Studies 2020 Time Series
+#'
+#' The ANES Time Series Study collects data on US voters' views on a range of political issues prior to and following a US Presidential Election.
+#'
+#' American National Election Studies. 2021. ANES 2020 Time Series Study Full Release. February 10, 2022 version. www.electionstudies.org
+#'
+#' @format ## `nes`
+#' A data frame with 8280 rows and 3 columns:
+#'
+#' \describe{
+#'    \item{respondent_id}{Each respondent's identification number}
+#'    \item{attention_to_politics}{How often do you pay attention to what’s going on in government and politics?}
+#'    \item{education}{What is the highest level of school you have completed or the highest degree you have received?}
+#' }
+#'
+#' @source https://electionstudies.org/data-center/2020-time-series-study/
+"nes"
---
+++ data-raw/nes.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+nes_raw <- read_csv(here::here("data-raw", "anes_timeseries_2020_csv_20220210.csv"))
+
+nes <- nes_raw |>
+  transmute(respondent_id = V200001,
+            attention_to_politics = factor(V201005,
+                                           label = c("Refused",
+                                                     "Always",
+                                                     "Most of the time",
+                                                     "About half the time",
+                                                     "Some of the time",
+                                                     "Never")),
+            education = factor(V201511x,
+                               label = c("Refused",
+                                         "Don't know",
+                                         NA_character_,
+                                         "Less than high school credential",
+                                         "High school credential",
+                                         "Some post-high school, no bachelor’s degree",
+                                         "Bachelor’s degree",
+                                         "Graduate degree")))
+
+usethis::use_data(nes, overwrite = TRUE)
